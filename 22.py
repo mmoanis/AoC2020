7358f73d1a8e1e5c0b4e31781bc4a51849749ce7
Mohamed Moanis <7101613+mmoanis@users.noreply.github.com>
2020-12-22 20:06:19 +0000

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,48 +1,61 @@
 import fileinput
+import re
+from copy import deepcopy
+
+def score(cards):
+    print(sum(cards[i]*(len(cards)-i) for i in range(len(cards))))
+
+
+def game1(cards1, cards2):
+    while len(cards1) > 0 and len(cards2) > 0:
+        if cards1[0] > cards2[0]:
+            cards1.append(cards1[0])
+            cards1.append(cards2[0])
+        elif cards1[0] < cards2[0]:
+            cards2.append(cards2[0])
+            cards2.append(cards1[0])
+        cards1.pop(0)
+        cards2.pop(0)
+
+    cards = cards1 if len(cards1) > 0 else cards2
+    score(cards)
+
+
+def game2(cards1, cards2):
+    seen1, seen2 = set(), set()
+    while len(cards1) > 0 and len(cards2) > 0:
+        if str(cards1) in seen1 or str(cards2) in seen2:
+            return True
+        
+        seen1.add(str(cards1))
+        seen2.add(str(cards2))
+
+        if len(cards1) > cards1[0] and len(cards2) > cards2[0]:
+            p1 = game2(cards1[1:cards1[0]+1], cards2[1:cards2[0]+1])
+        else: p1 = cards1[0] > cards2[0]
+
+        if p1:
+            cards1.append(cards1[0])
+            cards1.append(cards2[0])
+        else:
+            cards2.append(cards2[0])
+            cards2.append(cards1[0])
+        cards1.pop(0)
+        cards2.pop(0)
+    return len(cards1) > len(cards2)
 
 Lines = list([l.strip() for l in fileinput.input()])
-ingreds = {}
-allergs = {}
+cards1, cards2 = [], []
+cards = cards1
 for l in Lines:
-    items = l.split(' ')
-    ll = []
-    for item in items:
-        if '(contains' in item: continue
-        elif ',' in item or ')' in item:
-            allergy = item[:-1]
-            if allergy in allergs:
-                foods = allergs[allergy][:]
-                for food in allergs[allergy]:
-                    if food not in ll:
-                        foods.remove(food)
-                allergs[allergy] = foods
-            else:
-                allergs[allergy] = ll
-        else:
-            ll.append(item)
-            if item in ingreds: ingreds[item]+=1
-            else: ingreds[item] = 1
+    if 'Player 1:' in l: continue
+    elif 'Player 2:' in l:
+        cards = cards2
+    elif l:
+        cards.append(int(l))
 
-todo = allergs.keys()
-while True:
-    skip = True
-    for k,v in allergs.items():
-        if k in todo and len(v) == 1:
-            skip = False
-            K, V = k, v[0]
-            todo.remove(k)
-            break
-    
-    if skip: break
 
-    for k in allergs.keys():
-        if k == K: continue
-        if V in allergs[k]:
-            allergs[k].remove(V)
-
-ident = set()
-for k, v in allergs.items():
-    for vv in v: ident.add(vv)
-
-print(sum(ingreds[k] for k in ingreds.keys() if k not in ident))
-print(','.join(allergs[x][0] for x in sorted(allergs.keys())))
+game1(cards1[:], cards2[:])
+game2(cards1, cards2)
+score(cards1)
+score(cards2)
